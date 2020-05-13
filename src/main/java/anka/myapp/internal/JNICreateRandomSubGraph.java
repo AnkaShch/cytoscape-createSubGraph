0336b59e31515ccc861755f793b7f5137b27b975
AnkaShch <shchetsova.au@yandex.ru>
2020-05-13 16:17:15 +0300

    playing with naitive-lib-loader with Alex

--- src/main/java/anka/myapp/internal/JNICreateRandomSubGraph.java
+++ src/main/java/anka/myapp/internal/JNICreateRandomSubGraph.java
@@ -1,11 +1,20 @@
 package anka.myapp.internal;
 
+import org.scijava.nativelib.NativeLoader;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class JNICreateRandomSubGraph {
     public static native ArrayList<String> getSubGraphNodes(ArrayList<String> nodes, ArrayList<String> edges);
 
     static {
-        System.load("/home/anka/practic/CreatorRandomSubGraph/lib/randSubGraph.so");
+        //System.load("/home/anka/practic/CreatorRandomSubGraph/lib/randSubGraph.so");
+        try {
+
+            NativeLoader.loadLibrary("randSubGraph");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
